af3f933a56af8c3f9956434a8b47f688a61da308
Кирилл Попов <diplomatgmg@gmail.com>
2023-11-08 00:47:24 +0300

    add ignore patterns js

--- src/script.ts
+++ src/script.ts
@@ -1,1 +1,15 @@
-console.log('test')
+interface User {
+  firstName: string
+  age: number
+}
+
+function getArrayUser ({ firstName, age }: User): [string, number] {
+  return [firstName, age]
+}
+
+const user: User = {
+  firstName: 'Kirill',
+  age: 19
+}
+
+console.log(getArrayUser(user))
